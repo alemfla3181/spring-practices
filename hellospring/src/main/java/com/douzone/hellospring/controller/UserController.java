71893e203b4fe139b9d1d510b0fba228bde3d053
Dragon <alemfla3181@naver.com>
2022-06-03 11:24:14 +0900

    @RequestMapping #2

---
+++ hellospring/src/main/java/com/douzone/hellospring/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.douzone.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@RequestMapping(value = "/join", method=RequestMethod.GET)
+	public String join() {
+		return "/WEB-INF/views/join.jsp";
+	}
+	
+	@RequestMapping(value = "/join", method=RequestMethod.POST)
+	public String join(String name, String email, String password) {
+		return "redirect:/";
+	}
+}
