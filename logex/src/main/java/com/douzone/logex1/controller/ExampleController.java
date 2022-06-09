069e7563a6b2fc46c8bdd44ce8fc94921a69b39e
Dragon <alemfla3181@naver.com>
2022-06-09 11:22:13 +0900

    logex #finish

--- logex/src/main/java/com/douzone/logex1/controller/ExampleController.java
+++ logex/src/main/java/com/douzone/logex1/controller/ExampleController.java
@@ -12,18 +12,16 @@ public class ExampleController {
 	/**
 	 *  Logger 생성
 	 */
-	private static final Log LOG = LogFactory.getLog( ExampleController.class );
+	private static final Log LOG = LogFactory.getLog(ExampleController.class);
 	
-	@RequestMapping( "/ex1" )
+	@RequestMapping("/ex1")
 	@ResponseBody
 	public String ex1() {
-		
 		/**
 		 *  이 예제에서는 logback.xml 에서,
 		 *  
 		 *  1. consoleAppender 와 root logger 만 세팅해서 테스트 해 볼 수 있습니다. 
 		 *  2. root logger의 level="DEBUG" 조정해 봅니다.
-		 *  
 		 *  3. 테스트 해보면,
 		 *     DEBUG  > INFO > WARN > ERROR 순으로 로그가 출력 되는 것을 확인할
 		 *     수 있습니다.
@@ -32,10 +30,10 @@ public class ExampleController {
 		 *     직접 테스트 해 보세요.
 		 *  
 		 */
-		LOG.debug( "#ex1 - debug log" );
-		LOG.info( "#ex1 - info log" );
-		LOG.warn( "#ex1 - warn log" );
-		LOG.error( "#ex1 - error log" );
+		LOG.debug("#ex1 - debug log");
+		LOG.info("#ex1 - info log");
+		LOG.warn("#ex1 - warn log");
+		LOG.error("#ex1 - error log");
 		
 		return "Logback Logging Example1";
 	}
