717af30a565c803942597e3eb7c9d62765fece2b
Dragon <alemfla3181@naver.com>
2022-06-15 16:21:30 +0900

    Explicity(XML) Configuration

--- container/src/main/java/com/douzone/container/videosystem/DVDPlayer.java
+++ container/src/main/java/com/douzone/container/videosystem/DVDPlayer.java
@@ -1,9 +1,19 @@
 package com.douzone.container.videosystem;
 
 public class DVDPlayer {
-	
 	private DigitalVideoDisc dvd;
 	
+	public DVDPlayer() {
+	}
+	
+	public DVDPlayer(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+	
+	public void setDvd(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+
 	public String play() {
 		return dvd.play();
 	}
